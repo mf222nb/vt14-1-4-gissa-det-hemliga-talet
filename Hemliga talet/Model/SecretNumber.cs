19012ee4b66a659aebffe87dfa0b142414b49f49
mf222nb <mf222nb@student.lnu.se>
2014-02-04 19:13:34 +0100

    kommit en bit

---
+++ Hemliga talet/Model/SecretNumber.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+
+namespace Hemliga_talet.Model
+{
+    public enum Outcome
+    {
+        Indefine,
+        Low,
+        High,
+        Correct,
+        NoMoreGuesses,
+        PreviousGuess
+    }
+
+    public class SecretNumber: IEnumerable
+    {
+        private int _number;
+        private List<int> _previousGuesses;
+        public const int MaxNumberOfGuesses = 7;
+
+        //Egenskaper
+        public bool CanMakeGuess 
+        {
+            get
+            {
+                if (_previousGuesses.Count == MaxNumberOfGuesses)
+                {
+                    Outcome = Outcome.NoMoreGuesses;
+                    
+                    return false;
+                };
+                return true;
+            }
+        }
+        
+        public int Count 
+        {
+            get { return _previousGuesses.Count;} 
+        }
+
+        public int? Number 
+        {
+            get 
+            {
+                if (CanMakeGuess)
+                {
+                    return null;
+                }
+                else
+                {
+                    return _number;
+                }
+            } 
+        }
+        
+        public Outcome Outcome { get; set; }
+        
+        public IEnumerable<int> PreviousGuesses 
+        {
+            get { return _previousGuesses.AsReadOnly(); }
+        }
+
+        //Metoder
+        public void Initialize()
+        {
+            Random random = new Random();
+            _number = random.Next(1, 101);
+
+            _previousGuesses.Clear();
+            Outcome = Outcome.Indefine;
+        }
+
+        public Outcome MakeGuess(int guess)
+        {
+            return Outcome;
+        }
+
+        public SecretNumber()
+        {
+            _previousGuesses = new List<int>(MaxNumberOfGuesses);
+            Initialize();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
